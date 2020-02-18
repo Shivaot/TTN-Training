11f6e57b9764ae3cba133b1fea1fcd4f3bd4c708
Shiva Tiwari <shivatiwari757@gmail.com>
2020-02-18 18:54:39 +0530

    Updated Ans11 accordig to inner class

--- src/com/tothenew/Ans11.java
+++ src/com/tothenew/Ans11.java
@@ -2,13 +2,16 @@ package com.tothenew;
 
 public class Ans11 {
     public static void main(String[] args) {
+        Bank.SBI sbi = new Bank().new SBI();
+        Bank.BOI boi = new Bank().new BOI();
+        Bank.ICICI icici = new Bank().new ICICI();
         Bank bank = new Bank();
         System.out.println(bank.getDetails());
-        SBI sbi = new SBI();
+
         System.out.println(sbi.getDetails());
-        BOI boi = new BOI();
+
         System.out.println(boi.getDetails());
-        ICICI icici = new ICICI();
+  
         System.out.println(icici.getDetails());
     }
 
@@ -19,34 +22,29 @@ class Bank{
     public float getDetails() {
         return generalRateOfInterest;
     }
-
-}
-
-class BOI extends Bank{
-    float rateOfInterestBOI = 2.5f;
-
-    @Override
-    public float getDetails() {
-        return rateOfInterestBOI;
-    }
-}
-
-class SBI extends Bank{
-    float rateOfInterestSBI = 3.5f;
-
-    @Override
-    public float getDetails() {
-        return rateOfInterestSBI;
+    class BOI{
+        float rateOfInterestBOI = 2.5f;
+        public float getDetails() {
+            return rateOfInterestBOI;
+        }
     }
 
-}
+    class SBI{
+        float rateOfInterestSBI = 3.5f;
 
-class ICICI extends Bank{
-    float rateOfInterestICICI = 1.5f;
+        public float getDetails() {
+            return rateOfInterestSBI;
+        }
 
-    @Override
-    public float getDetails() {
-        return rateOfInterestICICI;
+    }
+
+    class ICICI{
+        float rateOfInterestICICI = 1.5f;
+
+        public float getDetails() {
+            return rateOfInterestICICI;
+        }
+
     }
 
 }
